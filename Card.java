8158c0dc53df074fb9d6204ef3ad2efa64b2a9cc
Naman Mittal <mittalnaman2005@gmail.com>
2025-03-10 14:11:16 +0530

    Commit with changes and solved errors

---
+++ Card.java
@@ -0,0 +1,22 @@
+class Card {
+	private String rank;
+	private String suit;
+
+	public Card(String rank, String suit) {
+		this.rank = rank;
+		this.suit = suit;
+	}
+
+	public String getRank() {
+		return rank;
+	}
+
+	public String getSuit() { // Added this method
+		return suit;
+	}
+
+	@Override
+	public String toString() {
+		return rank + " of " + suit;
+	}
+}
